8515217391f1cbf866514c7c10ce2ee54ad9a50d
ninelamericanel <ninelamericanel@gmail.com>
2022-03-22 18:10:13 +0300

    created structure of footer component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,5 +2,19 @@ import React from "react";
 import './style.css';
 
 export const Footer:React.FC = () => {
-    return <div> </div>
+    return (<footer className="footer">
+        <span className="todo-count">1 items left</span>
+        <ul className="filters">
+            <li>
+                <button className="selected">All</button>
+            </li>
+            <li>
+                <button>Active</button>
+            </li>
+            <li>
+                <button>Completed</button>
+            </li>
+        </ul>
+        <button className="clear-completed">Clear completed</button>
+    </footer>)
 }
