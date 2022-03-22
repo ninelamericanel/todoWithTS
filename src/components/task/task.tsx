db0935fd5b1010dd27b2c7c1c3eaf026ec8f3a25
ninelamericanel <ninelamericanel@gmail.com>
2022-03-22 18:01:30 +0300

    created items of todo list

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import './style.css';
 
-export const Task: React.FC = () => {
-    return <div></div>
+type TodoProp = {
+    todo: {
+        created: Date,
+        description: string
+    }
+}
+
+export const Task = ({todo}: TodoProp) => {
+    console.log(typeof todo);
+    let {description} = todo;
+    return (
+        <div className="view">
+            <input className="toggle" type="checkbox"></input>
+            <label>
+                <span className="description">{description}</span>
+                <span className="created"></span>
+            </label>
+            <button className="icon icon-edit"></button>
+            <button className="icon icon-destroy"></button>
+        </div>
+    // <input type="text" className="edit" value={description}></input>
+    )
 }
