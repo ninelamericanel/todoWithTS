8ed2848c746a8039e3977007dab1863810fce25b
ninelamericanel <ninelamericanel@gmail.com>
2022-03-22 18:12:56 +0300

    created structure of newTaskForm component

--- src/components/newTaskForm/newTaskForm.tsx
+++ src/components/newTaskForm/newTaskForm.tsx
@@ -2,5 +2,10 @@ import React from "react";
 import './style.css';
 
 export const NewTaskForm: React.FC = () => {
-    return <div></div>
+    return (
+        <header className="header">
+            <h1>todos</h1>
+            <input className="new-todo" placeholder="What needs to be done?" autoFocus></input>
+        </header>
+    )
 }
