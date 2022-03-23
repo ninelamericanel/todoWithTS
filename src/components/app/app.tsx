b1e693d40fc58860af26bb2b979034ff1b19486e
ninelamericanel <ninelamericanel@gmail.com>
2022-03-23 20:15:45 +0300

    modified App component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,22 +1,46 @@
-import React from "react";
+import React, {Component} from "react";
 import './style.css';
 import {TodoItem} from "../todos";
 import Footer from "../footer";
 import NewTaskForm from "../newTaskForm";
 import TaskList from "../taskList";
 
-export const App: React.FC = () => {
-    const todos: Array<TodoItem> = [
-        {id: "1", completed: true, created: new Date('December 17, 2021'), description: 'Completed task', editing: false},
-        {id: "2", completed: false, created: new Date('December 23, 2022'), description: 'Editing task', editing: true},
-        {id: "3", completed: false, created: new Date('December 25, 2022'), description: 'Active task', editing: false}]
+export default class App extends Component {
+    state = {
+        todos: [
+            {
+                id: "1",
+                completed: true,
+                created: new Date('December 17, 2021'),
+                description: 'Completed task',
+                editing: false
+            },
+            {
+                id: "2",
+                completed: false,
+                created: new Date('December 23, 2022'),
+                description: 'Editing task',
+                editing: true
+            },
+            {
+                id: "3",
+                completed: false,
+                created: new Date('December 25, 2022'),
+                description: 'Active task',
+                editing: false
+            }
+        ]
+    }
 
-    return (
-        <section className="todoapp">
-            <NewTaskForm/>
-            <section className='main'>
-                <TaskList todos={todos}/>
-            </section>
-            <Footer/>
-        </section>)
+    render() {
+        return (
+            <section className="todoapp">
+                <NewTaskForm/>
+                <section className="main">
+                    <TaskList todos={this.state.todos}/>
+                </section>
+                <Footer/>
+            </section>)
+    }
+
 }
