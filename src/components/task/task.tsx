31aa0f6a289a9ea85217107ef793d2083a954126
ninelamericanel <ninelamericanel@gmail.com>
2022-03-23 19:37:20 +0300

    modified type of TodoProp in Task component

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -2,15 +2,11 @@ import React from "react";
 import './style.css';
 
 type TodoProp = {
-    todo: {
         created: Date,
         description: string
-    }
 }
 
-export const Task = ({todo}: TodoProp) => {
-    console.log(typeof todo);
-    let {description} = todo;
+export const Task = ({created, description} : TodoProp) => {
     return (
         <div className="view">
             <input className="toggle" type="checkbox"></input>
