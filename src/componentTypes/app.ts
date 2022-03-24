db5d77e13d650fef8e884115bb8f319c9dfa0e17
ninelamericanel <ninelamericanel@gmail.com>
2022-03-24 18:18:04 +0300

    created type of OnActiveFilterFunc

--- src/componentTypes/app.ts
+++ src/componentTypes/app.ts
@@ -5,3 +5,5 @@ export type OnDeletedFunc = (id: string) => void
 export type OnAddFunc = (text: string) => void
 
 export type ClearCompleteFunc = () => void
+
+export type OnActiveFilterFunc = (name: string) => void
