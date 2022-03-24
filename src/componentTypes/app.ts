ff5bf1fb48fd457a6111ce42f3b252cacb7334c2
ninelamericanel <ninelamericanel@gmail.com>
2022-03-24 17:16:38 +0300

    added type of clear completed func

--- src/componentTypes/app.ts
+++ src/componentTypes/app.ts
@@ -3,3 +3,5 @@ export type OnCompletedFunc = (id: string) => void
 export type OnDeletedFunc = (id: string) => void
 
 export type OnAddFunc = (text: string) => void
+
+export type ClearCompleteFunc = () => void
