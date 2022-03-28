50a7e29e6b966468e315fd7fda90b621e2c7c355
ninelamericanel <ninelamericanel@gmail.com>
2022-03-28 10:51:36 +0300

    create function for handle keydown

--- src/components/newTaskForm/newTaskForm.tsx
+++ src/components/newTaskForm/newTaskForm.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import {OnAddFunc} from "@componentTypes/app";
 import './style.css';
 
-export const NewTaskForm: React.FC = () => {
+type NewTaskFormProps = {
+    onAdd: OnAddFunc
+}
+
+type HandleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => void
+
+export const NewTaskForm: React.FC<NewTaskFormProps> = ({onAdd}) => {
+
+    let handleKeyDown: HandleKeyDown = (event) => {
+        const {target} = event;
+        if (event.key === 'Enter') {
+            onAdd((target as HTMLButtonElement).value);
+            (target as HTMLButtonElement).value = ''
+        }
+    }
+
     return (
         <header className="header">
             <h1>todos</h1>
-            <input className="new-todo" placeholder="What needs to be done?" autoFocus></input>
+            <input className="new-todo"
+                   onKeyUp={handleKeyDown}
+                   placeholder="What needs to be done?"
+                   autoFocus></input>
         </header>
     )
 }
