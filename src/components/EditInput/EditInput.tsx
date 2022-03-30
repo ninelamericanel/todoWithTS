06ad53f4e3f069b345691c86b25346f3056acb85
ninelamericanel <ninelamericanel@gmail.com>
2022-03-30 17:46:05 +0300

    create new component for editing task

---
+++ src/components/EditInput/EditInput.tsx
@@ -0,0 +1,45 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+import { EditingTaskFunc } from 'types/app';
+
+interface EditInputProps {
+  editingTask: EditingTaskFunc;
+  description: string;
+  id: string;
+}
+
+type HandleChange = (event: React.ChangeEvent) => void;
+type HandleClick = (event: React.KeyboardEvent) => void;
+
+const EditInput: React.FC<EditInputProps> = ({ description, editingTask, id }) => {
+  const [value, setValue] = useState(description);
+  const inputElement = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputElement.current) {
+      inputElement.current.focus();
+    }
+  }, []);
+
+  const handleChange: HandleChange = (event) => {
+    const { target } = event;
+    const eventValue = (target as HTMLButtonElement).value;
+    setValue(eventValue);
+  };
+
+  const handleClick: HandleClick = (event) => (event.key === 'Enter' ? editingTask(value, id) : null);
+
+  return (
+    <input
+      type="text"
+      ref={inputElement}
+      className="edit"
+      value={value}
+      onChange={handleChange}
+      onKeyUp={handleClick}
+      autoFocus
+    ></input>
+  );
+};
+
+export default EditInput;
