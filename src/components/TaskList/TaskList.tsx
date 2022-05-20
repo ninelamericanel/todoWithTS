1ebff5974c78d8d19d90b468945c1e94b31ab519
Ni <ninelamericanel@gmail.com>
2022-05-20 19:14:25 +0300

    refactor

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,57 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import './TaskList.scss';
-import {
-  EditingTaskFunc,
-  OnChangeTimerFunc,
-  OnCompletedFunc,
-  OnDeletedFunc,
-  OnEditingFunc,
-  TimerFormatFunc,
-} from 'types/app';
-import { TodoItem } from 'types/todos';
+import { PropsContext } from 'context/props-context';
+import { Todo } from 'types/todos';
 import { Task } from 'components/Task';
 
-interface TodosProp {
-  todos: TodoItem[];
-  onCompleted: OnCompletedFunc;
-  onDeleted: OnDeletedFunc;
-  onEditing: OnEditingFunc;
-  editingTask: EditingTaskFunc;
-  timerFormat: TimerFormatFunc;
-  onChangeTimer: OnChangeTimerFunc;
-}
+interface TodosProp {}
 
-const TaskList: React.FC<TodosProp> = ({
-  onChangeTimer,
-  todos,
-  onCompleted,
-  onDeleted,
-  onEditing,
-  editingTask,
-  timerFormat,
-}) => {
-  const todosNodes = todos.map((item) => {
-    const { id, created, description, status, display, timer } = item;
+const TaskList: React.FC<TodosProp> = () => {
+  const context = useContext(PropsContext);
+  const todosNodes = context.todos.map((todo: Todo) => {
+    const { id, status, display } = todo;
     let classList = '';
     if (status === 'completed') classList += 'completed';
     if (!display) classList += ' display-none';
-    if (status === 'editing') classList = 'editing';
+    if (status === 'edit') classList = 'editing';
     return (
       <li className={classList} key={id}>
-        <Task
-          onEditing={onEditing}
-          created={created}
-          description={description}
-          status={status}
-          onDeleted={onDeleted}
-          onCompleted={onCompleted}
-          editingTask={editingTask}
-          id={id}
-          timer={timer}
-          timerFormat={timerFormat}
-          onChangeTimer={onChangeTimer}
-        />
+        <Task />
       </li>
     );
   });
