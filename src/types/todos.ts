025329996299fbfd1b79edff01c5246f150e2da4
Ni <ninelamericanel@gmail.com>
2022-05-24 13:25:12 +0300

    refactored

--- src/types/todos.ts
+++ src/types/todos.ts
@@ -1,22 +1,4 @@
-import {PropsContext} from "../context/props-context";
-import React from "react";
-
-export interface TodoItem {
-  description: string;
-  id: string;
-  created: Date;
-  status: string;
-  display: boolean;
-  timer: {
-    min: string;
-    sec: string;
-  };
-}
-
-export interface FilterButtons {
-  name: string;
-  selected: boolean;
-}
+import React from 'react';
 
 export interface Todo {
   id: string;
@@ -24,8 +6,8 @@ export interface Todo {
   description: string;
   display: boolean;
   status: string;
-  min: string;
-  sec: string;
+  initialMin: string;
+  initialSec: string;
 }
 
 export interface PropsContextType {
@@ -39,31 +21,16 @@ export interface PropsContextType {
 }
 
 export type NoParamsVoidFunc = () => void;
-
 export type CreateNewTaskFunc = (description: string, min: string, sec: string) => Todo;
-
 export type FindMaxIdFunc = () => string;
 export type DisplayTodoFunc = () => boolean;
 export type OnChangeStatusFunc = (id: string, status: string) => void;
-
 export type OnDeletedFunc = (id: string) => void;
-
 export type OnAddTodoFunc = (name: string, min: string, sec: string) => void;
-
-export type ClearCompleteFunc = () => void;
-
-export type OnSelectedFilterFunc = (name: string) => void;
-
-export type OnFilterTodosFunc = (name: string) => void;
-
-export type OnEditingFunc = (id: string) => void;
-
+export type OnFilterTodosFunc = (name?: string) => void;
 export type EditingTaskFunc = (value: string, id: string) => void;
-
 export type TimerFormatFunc = (num: number) => string;
-
 export type OnChangeTimerFunc = (min: string, sec: string, id: string) => void;
-
 export type HandleKeyUpFunc = (event: React.KeyboardEvent<HTMLFormElement>) => void;
 export type HandleChangeFunc = (event: React.ChangeEvent<HTMLInputElement>) => void;
 export type HandleKeyUpInputFunc = (event: React.KeyboardEvent<HTMLInputElement>) => void;
