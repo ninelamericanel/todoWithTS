3267d4c7286c0e640e539346aedbde57807b4dff
Ni <ninelamericanel@gmail.com>
2022-05-24 13:28:26 +0300

    refactored

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,36 +1,43 @@
-import React, { FC, useContext, useState } from 'react';
+import React, { FC, useContext, useEffect, useState } from 'react';
 import './Task.scss';
 import { formatDistanceToNow } from 'date-fns';
 
 import { EditInput } from 'components/EditInput';
 import { Timer } from 'components/Timer';
 import { PropsContext } from 'context/props-context';
-import { NoParamsVoidFunc, Todo } from 'types/todos';
+import { DisabledButtonPlayFunc, NoParamsVoidFunc, Todo } from 'types/todos';
 
 interface TaskProps {
   todo: Todo;
 }
 
-const Task: FC<TaskProps> = ({ todo: { id, created, sec, min, description, status } }) => {
+const Task: FC<TaskProps> = ({ todo: { id, created, initialSec, initialMin, description, status } }) => {
   const [play, setPlay] = useState(false);
-  const { onChangeStatusFunc, onDeletedFunc } = useContext(PropsContext);
+  const [timer, setTimer] = useState({
+    timerMin: initialMin,
+    timerSec: initialSec,
+  });
+  const { timerMin, timerSec } = timer;
+  const { onChangeTimerFunc, onChangeStatusFunc, onDeletedFunc, onFilterTodosFunc } = useContext(PropsContext);
   const date = formatDistanceToNow(new Date(created), { includeSeconds: true });
   const completed: boolean = status === 'completed';
+  useEffect(() => onChangeTimerFunc(timerMin, timerSec, id), [play]);
   const turnOnTimer: NoParamsVoidFunc = () => {
     setPlay(false);
     onChangeStatusFunc(id, 'completed');
   };
-  const disabledButtonPlay = (): boolean => (min === '00' && sec === '00') || completed;
+  const disabledButtonPlay: DisabledButtonPlayFunc = () =>
+    (initialMin === '00' && initialSec === '00') || (timerSec === '00' && timerMin === '00') || completed;
   const editing = status === 'editing' ? <EditInput id={id} description={description} /> : null;
-  const timer = play ? (
+  const timerView = play ? (
     <>
       <button onClick={() => setPlay(false)} className="icon icon-pause"></button>
-      <Timer id={id} turnOnTimer={turnOnTimer} min={min} sec={sec} />
+      <Timer turnOnTimer={turnOnTimer} timer={timer} setTimer={setTimer} />
     </>
   ) : (
     <>
       <button onClick={() => setPlay(true)} disabled={disabledButtonPlay()} className="icon icon-play"></button>
-      {min}:{sec}
+      {timerMin}:{timerSec}
     </>
   );
 
@@ -41,7 +48,10 @@ const Task: FC<TaskProps> = ({ todo: { id, created, sec, min, description, statu
           className="toggle"
           type="checkbox"
           checked={completed}
-          onChange={() => onChangeStatusFunc(id, 'completed')}
+          onChange={() => {
+            onChangeStatusFunc(id, 'completed');
+            onFilterTodosFunc();
+          }}
         ></input>
         <div className="label">
           <span
@@ -49,11 +59,12 @@ const Task: FC<TaskProps> = ({ todo: { id, created, sec, min, description, statu
             onClick={() => {
               onChangeStatusFunc(id, 'completed');
               setPlay(false);
+              onFilterTodosFunc();
             }}
           >
             {description}
           </span>
-          <span className="description">{timer}</span>
+          <span className="description">{timerView}</span>
           <span className="description">created {date} ago</span>
         </div>
         <button
